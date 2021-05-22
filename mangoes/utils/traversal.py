f48fc5888e5801af5f170e01460d456952a48a07
Nami Akazawa <nakazawa@frontend-buster>
2021-05-22 17:01:26 +0200

    Change DFS from recursive to iterative.

--- mangoes/utils/traversal.py
+++ mangoes/utils/traversal.py
@@ -6,58 +6,63 @@ def find_parents(count, depth, idx, label, sentence_tree, parents, visited):
 
     Parameters
     -----------
-    count: int
-    depth: int
+    count: int. Denotes how far away from current node.
+    depth: int. Denotes maximal depth.
     idx: int
         index of word in a sentence.
-    label: str
+    label: str. Contains dependency relation.
     sentence_tree: list
-    parents: set
-    visited: set
+    parents: set. Append all nodes that are reachable for idx. 
+    visited: set. 
     """
+    stack = [(idx, label, count)]
 
-    if count > depth:
-        return 
+    while stack:
+        idx, label, count = stack.pop()
+        # print(idx, label, count)
+        if count > depth:
+            return 
 
-    if idx == 0:
-        parents.add((idx, "", count))
-    else:
         parents.add((idx, label, count))
-
-    visited.add(idx)
-    for token, token_children in enumerate(sentence_tree):
-        for child, child_label in token_children:
-                if child == idx:
+        for token, token_children in enumerate(sentence_tree):
+            for child, child_label in token_children:
+                if child == idx and idx not in visited:
+                    visited.add(idx)
                     new_label = label + "+" + child_label if label else child_label
-                    find_parents(count+1, depth, token, new_label, sentence_tree, parents, visited)
-        
+                    stack.append((token, new_label, count+1))
+   
 
 def dfs(remain, node, label, visited, new_children, sentence_tree):
     """Find path that connects current element up to depth-far.
 
     Parameters
     -----------
-    remain: int 
+    remain: int. How far node still can travarse. 
     node: int 
         index of word in a sentence.
-    label: str 
+    label: str. Contains dependency relation.
     visited: set 
     new_children: set 
-    sentence_tree: list(set)
+    sentence_tree: list.
     """
     if remain < 0:
         return 
-
-    if label:
-        new_children.add((node, label))
-
-    visited.add(node)
-
-    for j, token_children in enumerate(sentence_tree):
-        if j not in visited:
-            for child, child_label in token_children:
-                if child == node:
-                    new_label = label+ "+" + child_label if label else child_label
-                    dfs(remain-1, j, new_label, visited, new_children, sentence_tree)
     
-    
+    stack = [(remain, node, label)]
+
+    while stack:
+        remain, node, label = stack.pop()
+
+        if remain < 0:
+            continue 
+        
+        if label:
+            new_children.add((node, label))
+
+        for j, token_children in enumerate(sentence_tree):
+            if j not in visited:
+                for child, child_label in token_children:
+                    if child == node:
+                        visited.add(node)
+                        new_label = label+ "+" + child_label if label else child_label
+                        stack.append((remain-1, j, new_label))
